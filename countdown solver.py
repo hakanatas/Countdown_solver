e7bbd733aa9a0958ca8e17742a879f343c811360
Hakan Atas <hakanatas@gmail.com>
2019-08-13 14:25:12 +0300

    Update countdown solver.py

--- countdown solver.py
+++ countdown solver.py
@@ -1,55 +1,29 @@
+#https://github.com/lse30/Countdown_solver/blob/master/countdown%20solver.py
+
 import time
+import random
+import re
+from itertools import permutations
+from itertools import combinations_with_replacement
+from fpdf import FPDF
+
+pdf = FPDF()
+pdf.add_page()
+
+
+pdf_cozumlu = FPDF()
+pdf_cozumlu.add_page()
 
 def solver(numbers, target):
     start_time = time.time()
     num_orders = []
     used_nums = []
 
-    for first in range(0,6):
-
-        used_nums.append(first)
-
-        for second in range(0,6):
-
-            if second not in used_nums:
-                used_nums.append(second)
-
-                for third in range(0,6):
-
-                    if third not in used_nums:
-                        used_nums.append(third)
-
-                        for forth in range(0,6):
-
-                            if forth not in used_nums:
-                                used_nums.append(forth)
-
-                                for fifth in range(0,6):
-
-                                    if fifth not in used_nums:
-                                        used_nums.append(fifth)
-
-                                        for sixth in range(0,6):
-
-                                            if sixth not in used_nums:
-                                                nums = [numbers[first],
-                                                        numbers[second],
-                                                        numbers[third],
-                                                        numbers[forth],
-                                                        numbers[fifth],
-                                                        numbers[sixth],]
-                                                num_orders.append(nums)
-
-                                        used_nums.remove(fifth)
-                                used_nums.remove(forth)
-                        used_nums.remove(third)
-                used_nums.remove(second)
-        used_nums.remove(first)
+    num_orders = list(permutations(number_list))
 
 
     operators = ['a','s','m','d']
     op_sequence = []
-
     for operator_1 in operators:
         for operator_2 in operators:
             for operator_3 in operators:
@@ -58,10 +32,11 @@ def solver(numbers, target):
                         op_list = [operator_1, operator_2, operator_3, operator_4, operator_5]
                         op_sequence.append(op_list)
 
+    op1_sequence = list(combinations_with_replacement(operators, 5))
 
     for num_order in num_orders:
-        for sequence in op_sequence:
-
+        for sequence in op1_sequence:
+            sequence = list(sequence)
             i = 0
             while i <= 5:
 
@@ -76,20 +51,31 @@ def solver(numbers, target):
                         current *= num_order[i]
                     else:
                         current /= num_order[i]
+
                 if current == target:
-                    start = str(target) + ' = ' + str(num_order[0])
+                    start = str(num_order[0])
+                    basla = num_order[0]
+
                     j = 0
                     while j < i:
+                        tmp = basla
                         if sequence[j] == 'a':
                             use = '+'
+                            basla = basla + num_order[j+1]
                         elif sequence[j] == 's':
                             use = '-'
+                            basla = basla - num_order[j+1]
                         elif sequence[j] == 'm':
                             use = '*'
+                            basla = basla * num_order[j+1]
                         else:
                             use = '/'
-                        string = use + ' ' + str(num_order[j+1])
-                        start += ' ' + string
+                            basla = basla / num_order[j+1]
+                        if j==0:
+                            string = use + str(num_order[j+1]) + "=" + str(basla) + ",     "
+                        else:
+                            string = str(tmp) + use + str(num_order[j+1]) + "=" + str(basla) + ",     "
+                        start += string
                         j += 1
                     time_elapsed = time.time()
                     print('time taken: ', (time_elapsed-start_time))
@@ -99,39 +85,71 @@ def solver(numbers, target):
 
                 i += 1
 
+# rastgele sayı ürettiğimiz bölüm
+def number_generator():
+    num_list = []
+    target = random.randint(200, 999)
+    num_list.append(random.randint(1, 10))
+    num_list.append(random.randint(1, 10))
+    num_list.append(random.randint(1, 10))
+    num_list.append(random.randint(1, 10))
+    num_list.append(random.randint(1, 4) * 25)
+    num_list.append(random.randint(1, 4) * 25)
+    return(target, num_list)
 
-"""This is the first attempt at making code that can generate every possible way of adding, subtracting
-multiplying and dividing numbers in attempt to find the target number
-it does not work for all numbers because this version does not try every possible combination
-for example the list [1, 6, 2, 10, 25, 100] and target 863 will return None however the solution is 
-863 = (10-1)*100-25-(2*6)
-this is impossible to create without using brackets and therefore cannot be made in the solver
-"""
+#ürettiğimiz sayıları pdf dökümanını yazıyoruz
+def write_pdf(target_num,nums,answers):
+    for i in range(len(target_num)):
 
-"""To use"""
-"""change the target variable to your target and set the number_list variable to
- your list of the 6 numbers that are given
-NOTE the show allows for duplicate numbers and so does the solver
-an example is shown below
-target = 864
-number_list = [1, 6, 2, 10, 25, 100]
-"""
+        #hedef sayıyı yaz
+        pdf.set_font('Times', 'B', 20)
+        pdf.cell(200, 15, txt=str(target_num[i]), ln=1, align="C")
 
-target = 864
-number_list = [1, 6, 2, 10, 25, 100]
+        pdf_cozumlu.set_font('Times', 'B', 20)
+        pdf_cozumlu.cell(200, 15, txt=str(target_num[i]), ln=1, align="C")
+        #kullanılacak sayilari yaz
+        pdf.set_font('Times', 'B', 14)
+        pdf_cozumlu.set_font('Times', 'B', 14)
 
-answer = solver(number_list, target)
-print(answer)
+        values = '                 '.join(str(v) for v in nums[i])
+        pdf.cell(200,10,txt= values, ln=1, align='C')
+        pdf_cozumlu.cell(200,10,txt= values, ln=1, align='C')
+        #çözümü yazar
+        cevap = 0
 
-"""the number is printed followed by the order of operations"""
-"""DO NOT APPLY BEDMAS, 
-    Simply read the inputs from left to right:
-    EXAMPLE: 864 = 2 * 10 - 1 + 25 + 100 * 6
-    implies:
-    2 * 10 = 20
-    20 - 1 = 19
-    19 + 25 = 44
-    44 + 100 = 144
-    144 * 6 = 864"""
+        pdf_cozumlu.cell(200,75, txt="",ln = 1, align='C')
+
+        pdf.cell(200, 105, txt="", ln=1, align='C')
+        pdf_cozumlu.cell(200, 10, txt=str(answers[i]), ln=1, align='C')
+
+        pdf_cozumlu.cell(200,20, txt="",ln = 1, align='C')
 
 
+
+# rastgele sayı üretimi biter
+
+#sayıları fonksiyonlara gönderip çözüm için beklediğimiz bölüm
+
+countdown_number = []
+countdown_target = []
+countdown_answer = []
+hedef, number_list = number_generator()
+answer = solver(number_list, hedef)
+
+for i in range(10): #range(10)--> generate 10 question
+
+    while answer == None:
+        number_list = []
+        hedef, number_list = number_generator()
+        answer = solver(number_list, hedef)
+
+    print(answer)
+    countdown_target.append(hedef)
+    countdown_number.append(number_list)
+    countdown_answer.append(answer)
+    hedef, number_list = number_generator()
+    answer = solver(number_list, hedef)
+
+write_pdf(countdown_target, countdown_number, countdown_answer)
+pdf.output('countdown.pdf')
+pdf_cozumlu.output('countdown_solutions.pdf')
